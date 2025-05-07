d455e045d1053cbd6a64c16929c24d7838fa9bea
zenoxart <81261706+zenoxart@users.noreply.github.com>
2025-05-07 18:20:57 +0200

    MenuHandler.cs -Skizze erstellt

---
+++ WPF/WPFBasics/Common/Handler/MenuHandler.cs
@@ -0,0 +1,33 @@
+namespace WPFBasics.Common.Handler {
+    /// <summary>
+    /// Stellt einen Menü-Handler für das Verwalten & Laden 
+    /// von dynamischen generierten Menü-Punkten
+    /// </summary>
+    public class MenuHandler {
+        
+        /// <summary>
+        /// Definiert das zu behandelnde Menü
+        /// TODO: einen RelayCommandPool benutzen
+        /// </summary>
+        public Menu Items {
+            get => field;
+            set {
+                if(field != value)
+                {
+                    field = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Läd die Menü-Items aus dem angegebenen Pfad eines Ressource-Dictionary
+        /// </summary>
+        public void LoadItemsFromResDict(string path){
+            /// Lade aus dem Resource-File
+
+            /// Überprüfe ob das Element noch nicht geladen wurde
+                /// Hinterlege die Commands sowie die Views in Menü-Items
+        }
+    }
+}
