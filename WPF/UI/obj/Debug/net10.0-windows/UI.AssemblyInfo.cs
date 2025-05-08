db6bf9d83d61f907cbbc84c52665580152e2aad1
U731233 <daniel.kasper@voestalpine.com>
2025-05-08 07:22:52 +0200

    Remove Example-Lib
    Remove Example-Lib & add gitignore

--- WPF/UI/obj/Debug/net10.0-windows/UI.AssemblyInfo.cs
+++ WPF/UI/obj/Debug/net10.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0deef67fd51b6d94842ddc9b76fd68c763f64014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d455e045d1053cbd6a64c16929c24d7838fa9bea")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WPF/WPFBasics/Common/Handler/MenuHandler.cs
+++ WPF/WPFBasics/Common/Handler/MenuHandler.cs
@@ -1,18 +1,25 @@
-namespace WPFBasics.Common.Handler {
+using System.Windows.Controls;
+using WPFBasics.Common.ViewModelSupport;
+
+namespace WPFBasics.Common.Handler
+{
     /// <summary>
     /// Stellt einen Menü-Handler für das Verwalten & Laden 
     /// von dynamischen generierten Menü-Punkten
     /// </summary>
-    public class MenuHandler {
-        
+    public class MenuHandler : ViewModelNotifyPropertyBase
+    {
+
         /// <summary>
         /// Definiert das zu behandelnde Menü
         /// TODO: einen RelayCommandPool benutzen
         /// </summary>
-        public Menu Items {
+        public Menu Items
+        {
             get => field;
-            set {
-                if(field != value)
+            set
+            {
+                if (field != value)
                 {
                     field = value;
                     OnPropertyChanged();
@@ -23,11 +30,12 @@ namespace WPFBasics.Common.Handler {
         /// <summary>
         /// Läd die Menü-Items aus dem angegebenen Pfad eines Ressource-Dictionary
         /// </summary>
-        public void LoadItemsFromResDict(string path){
+        public void LoadItemsFromResDict(string path)
+        {
             /// Lade aus dem Resource-File
 
             /// Überprüfe ob das Element noch nicht geladen wurde
-                /// Hinterlege die Commands sowie die Views in Menü-Items
+            /// Hinterlege die Commands sowie die Views in Menü-Items
         }
     }
 }
